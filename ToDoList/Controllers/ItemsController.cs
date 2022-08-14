e66b1ddb60a91cf8afadf05f5d9fef7ffa597c49
Nick Tse <nicholashtse@gmail.com>
2022-08-14 01:27:50 -0700

    update items edit view

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -49,20 +49,20 @@ namespace ToDoList.Controllers
         return View(thisItem);
     }
 
-//     public ActionResult Edit(int id)
-//     {
-//       var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-//       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-//       return View(thisItem);
-//     }
+    public ActionResult Edit(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem);
+    }
 
-//     [HttpPost]
-//     public ActionResult Edit(Item item)
-//     {
-//       _db.Entry(item).State = EntityState.Modified;
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+      _db.Entry(item).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
 //     public ActionResult Delete(int id)
 //     {
